5c20bfc8169e39247ea68e1a9ca51448492bd756
Vitabile <107575136+Vitabile@users.noreply.github.com>
2024-01-05 15:52:11 +0100

    enet pretrained and miou (table 3)

--- eval/dataset.py
+++ eval/dataset.py
@@ -13,41 +13,46 @@ from torch.utils.data import Dataset
 # allowed image extensions
 EXTENSIONS = ['.jpg', '.png']
 
+
 def load_image(file):
     # read an image
     return Image.open(file)
+
 
 def is_image(filename):
     # check is the given filename is a valid image file
     return any(filename.endswith(ext) for ext in EXTENSIONS)
 
+
 def is_label(filename):
     # check if the given filename is a valid label file
     return filename.endswith("_labelTrainIds.png")
+
 
 def image_path(root, basename, extension):
     # construct an image path
     return os.path.join(root, f'{basename}{extension}')
 
+
 def image_path_city(root, name):
     # construct an image path for cityscapes files
     return os.path.join(root, f'{name}')
+
 
 def image_basename(filename):
     # get the image filename without extension
     return os.path.basename(os.path.splitext(filename)[0])
 
-class VOC12(Dataset):
 
+class VOC12(Dataset):
     def __init__(self, root, input_transform=None, target_transform=None):
         # define the root directory containing all images
         self.images_root = os.path.join(root, 'images')
         # define the root directory containing all labels
         self.labels_root = os.path.join(root, 'labels')
 
-        # take all image filenames 
-        self.filenames = [image_basename(f)
-            for f in os.listdir(self.labels_root) if is_image(f)]
+        # take all image filenames
+        self.filenames = [image_basename(f) for f in os.listdir(self.labels_root) if is_image(f)]
         # sort the image filenames
         self.filenames.sort()
 
@@ -81,9 +86,7 @@ class VOC12(Dataset):
 
 
 class cityscapes(Dataset):
-
-    def __init__(self, root, input_transform=None, target_transform=None, subset='val'):
-        
+    def __init__(self, root, input_transform=None, target_transform=None, subset='val/'):
         # define the root directory containing all images
         self.images_root = os.path.join(root, 'leftImg8bit/' + subset)
         # define the root directory containing all labels
@@ -91,12 +94,16 @@ class cityscapes(Dataset):
         print(self.images_root, self.labels_root)
 
         # take all image filenames (dirpath + filename)
-        self.filenames = [os.path.join(dp, f) for dp, _, fn in os.walk(os.path.expanduser(self.images_root)) for f in fn if is_image(f)]
+        self.filenames = [
+            dp + '/' + f for dp, _, fn in os.walk(os.path.expanduser(self.images_root)) for f in fn if is_image(f)
+        ]
         # sort the image filenames
         self.filenames.sort()
 
         # take all label filenames (dirpath + filename)
-        self.filenamesGt = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(self.labels_root)) for f in fn if is_label(f)]
+        self.filenamesGt = [
+            dp + '/' + f for dp, dn, fn in os.walk(os.path.expanduser(self.labels_root)) for f in fn if is_label(f)
+        ]
         # sort the label filenames
         self.filenamesGt.sort()
 
@@ -111,13 +118,13 @@ class cityscapes(Dataset):
         # get a specific label filename
         filenameGt = self.filenamesGt[index]
 
-        #print(filename)
+        # print(filename)
 
         # read the corresponding RGB image
-        with open(image_path_city(self.images_root, filename), 'rb') as f:
+        with open(filename, 'rb') as f:
             image = load_image(f).convert('RGB')
         # read the corresponding label (palettised) image
-        with open(image_path_city(self.labels_root, filenameGt), 'rb') as f:
+        with open(filenameGt, 'rb') as f:
             label = load_image(f).convert('P')
 
         # perform a input image transformation if any
@@ -131,4 +138,3 @@ class cityscapes(Dataset):
 
     def __len__(self):
         return len(self.filenames)
-
--- eval/eval_iou.py
+++ eval/eval_iou.py
@@ -20,6 +20,7 @@ from torchvision.transforms import ToTensor, ToPILImage
 
 from dataset import cityscapes
 from erfnet import ERFNet
+from enet import ENet
 from transform import Relabel, ToLabel, Colorize
 from iouEval import iouEval, getColorEntry
 
@@ -27,18 +28,22 @@ NUM_CHANNELS = 3
 NUM_CLASSES = 20
 
 image_transform = ToPILImage()
-input_transform_cityscapes = Compose([
-    Resize(512, Image.BILINEAR),
-    ToTensor(),
-])
-target_transform_cityscapes = Compose([
-    Resize(512, Image.NEAREST),
-    ToLabel(),
-    Relabel(255, 19),   #ignore label to 19
-])
+input_transform_cityscapes = Compose(
+    [
+        Resize(512, Image.BILINEAR),
+        ToTensor(),
+    ]
+)
+target_transform_cityscapes = Compose(
+    [
+        Resize(512, Image.NEAREST),
+        ToLabel(),
+        Relabel(255, 19),  # ignore label to 19
+    ]
+)
+
 
 def main(args):
-
     if not os.path.exists('iou_results.txt'):
         open('iou_results.txt', 'w').close()
 
@@ -46,21 +51,21 @@ def main(args):
     modelpath = args.loadDir + args.loadModel
     weightspath = args.loadDir + args.loadWeights
 
-    print ("Loading model: " + modelpath)
-    print ("Loading weights: " + weightspath)
+    print("Loading model: " + modelpath)
+    print("Loading weights: " + weightspath)
 
     if modelname == "erfnet":
         model = ERFNet(NUM_CLASSES)
     elif modelname == "enet":
-        pass
+        model = ENet(NUM_CLASSES)
     elif modelname == "bisenet":
         pass
 
-    #model = torch.nn.DataParallel(model)
-    if (not args.cpu):
+    # model = torch.nn.DataParallel(model)
+    if not args.cpu:
         model = torch.nn.DataParallel(model).cuda()
 
-    def load_my_state_dict(model, state_dict):  #custom function to load model when not all dict elements
+    def load_my_state_dict(model, state_dict):  # custom function to load model when not all dict elements
         own_state = model.state_dict()
         for name, param in state_dict.items():
             if name not in own_state:
@@ -73,25 +78,33 @@ def main(args):
                 own_state[name].copy_(param)
         return model
 
-    model = load_my_state_dict(model, torch.load(weightspath, map_location=lambda storage, loc: storage))
-    print ("Model and weights LOADED successfully")
+    if modelname == 'enet':
+        model = load_my_state_dict(model.module, torch.load(weightspath)['state_dict'])
+    else:
+        model = load_my_state_dict(model, torch.load(weightspath, map_location=lambda storage, loc: storage))
 
+    # print(model.module.state_dict().eys())
+
+    print("Model and weights LOADED successfully")
 
     model.eval()
 
-    if(not os.path.exists(args.datadir)):
-        print ("Error: datadir could not be loaded")
+    if not os.path.exists(args.datadir):
+        print("Error: datadir could not be loaded")
 
-
-    loader = DataLoader(cityscapes(args.datadir, input_transform_cityscapes, target_transform_cityscapes, subset=args.subset), num_workers=args.num_workers, batch_size=args.batch_size, shuffle=False)
-
+    loader = DataLoader(
+        cityscapes(args.datadir, input_transform_cityscapes, target_transform_cityscapes, subset=args.subset),
+        num_workers=args.num_workers,
+        batch_size=args.batch_size,
+        shuffle=False,
+    )
 
     iouEvalVal = iouEval(NUM_CLASSES)
 
     start = time.time()
 
     for step, (images, labels, filename, filenameGt) in enumerate(loader):
-        if (not args.cpu):
+        if not args.cpu:
             images = images.cuda()
             labels = labels.cuda()
 
@@ -99,26 +112,27 @@ def main(args):
         with torch.no_grad():
             outputs = model(inputs)
 
+        outputs = torch.roll(outputs, -1, 1)
+
         iouEvalVal.addBatch(outputs.max(1)[1].unsqueeze(1).data, labels)
 
-        filenameSave = filename[0].split("leftImg8bit/")[1] 
+        filenameSave = filename[0].split("leftImg8bit/")[1]
 
-        print (step, filenameSave)
-
+        print(step, filenameSave)
 
     iouVal, iou_classes = iouEvalVal.getIoU()
 
     iou_classes_str = []
     for i in range(iou_classes.size(0)):
         # iouStr = getColorEntry(iou_classes[i])+'{:0.2f}'.format(iou_classes[i]*100) + '\033[0m'
-        iouStr = '{:0.2f}'.format(iou_classes[i]*100)
+        iouStr = '{:0.2f}'.format(iou_classes[i] * 100)
         iou_classes_str.append(iouStr)
 
     with open('iou_results.txt', 'a') as f:
         print("---------------------------------------", file=f)
-        print("Model", modelname.upper(), "Took", time.time()-start, "seconds", file=f)
+        print("Model", modelname.upper(), "Took", time.time() - start, "seconds", file=f)
         print("=======================================", file=f)
-        #print("TOTAL IOU: ", iou * 100, "%", file=f)
+        # print("TOTAL IOU: ", iou * 100, "%", file=f)
         print("Per-Class IoU:", file=f)
         print(iou_classes_str[0], "Road", file=f)
         print(iou_classes_str[1], "sidewalk", file=f)
@@ -141,18 +155,19 @@ def main(args):
         print(iou_classes_str[18], "bicycle", file=f)
         print("=======================================", file=f)
         # iouStr = getColorEntry(iouVal)+'{:0.2f}'.format(iouVal*100) + '\033[0m'
-        iouStr = '{:0.2f}'.format(iouVal*100)
+        iouStr = '{:0.2f}'.format(iouVal * 100)
         print("MEAN IoU: ", iouStr, "%", file=f)
+
 
 if __name__ == '__main__':
     parser = ArgumentParser()
 
     parser.add_argument('--state')
 
-    parser.add_argument('--loadDir',default="../trained_models/")
+    parser.add_argument('--loadDir', default="../trained_models/")
     parser.add_argument('--loadWeights', default="erfnet_pretrained.pth")
     parser.add_argument('--loadModel', default="erfnet.py")
-    parser.add_argument('--subset', default="val")  #can be val or train (must have labels)
+    parser.add_argument('--subset', default="val")  # can be val or train (must have labels)
     parser.add_argument('--datadir', default="/home/shyam/ViT-Adapter/segmentation/data/cityscapes/")
     parser.add_argument('--num-workers', type=int, default=4)
     parser.add_argument('--batch-size', type=int, default=1)
