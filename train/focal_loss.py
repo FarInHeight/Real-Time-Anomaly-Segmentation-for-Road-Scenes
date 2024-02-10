fix logit normalization

train/focal_loss.py
@@ -6,13 +6,13 @@ from torch.autograd import Variable
 
 class FocalLoss(nn.Module):
 
-    # TODO we need to weight init as cross entropoy loss?
+    # TODO we need to weight init as cross entropy loss?
 
-    def __init__(self, gamma=0, alpha=None, size_average=True):
+    def __init__(self, gamma=2, alpha=None, size_average=True):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
         self.alpha = alpha
-        if isinstance(alpha, (float, int, long)):
+        if isinstance(alpha, (float, int)):
             self.alpha = torch.Tensor([alpha, 1 - alpha])
         if isinstance(alpha, list):
             self.alpha = torch.Tensor(alpha)

train/logit_norm_loss.py
@@ -5,13 +5,13 @@ import torch.functional as F
 
 class LogitNormLoss(nn.Module):
 
-    def __init__(self, device, loss_func, t=1.0):
+    def __init__(self, device, loss_func, t=1.0):  # TODO manage Temperature parameter
         super(LogitNormLoss, self).__init__()
         self.device = device
         self.t = t
         self.loss_func = loss_func
 
     def forward(self, x, target):
-        norms = torch.norm(x, p=2, dim=-1, keepdim=True) + 1e-7
+        norms = torch.norm(x, p=2, dim=1, keepdim=True) + 1e-7
         logit_norm = torch.div(x, norms) / self.t
         return self.loss_func(logit_norm, target)